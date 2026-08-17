76fab34cdd15a80a5e72aec18d0f4b1090bcca84
Jxian718 <jxian0239@gmail.com>
2026-08-17 10:48:38 +0800

    Delete exersice.js

--- exersice.js
+++
@@ -1,7 +0,0 @@
-let fullName = "Jxian";
-let age = 19;
-let isStudent =true;
-
-let greeting = 'Hello, my name is ${fullName}, I am ${age} years old, and it is ${isStudent} that I am a student.'
-
-console.log(greeting); //Hello, my name is Jxian, I am 19 years old, and it is true I am a student.
