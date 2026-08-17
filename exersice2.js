689468628e3f902b35dd3addceb7628fb1d87ea4
Jxian718 <jxian0239@gmail.com>
2026-08-17 09:12:52 +0800

    Add files via upload

---
+++ exersice2.js
@@ -0,0 +1,20 @@
+function calculateArea(length,width) {return length*width}
+
+console.log("Area: " + calculateArea(5, 5));  // 25
+console.log("Area: " + calculateArea(5, 10)); // 50
+console.log("Area: " + calculateArea(10, 10)); // 100
+
+function checkTemparature(temparature) 
+{if (temparature > 30) 
+    {
+        return"Hot";}
+        else
+    {
+        return"Cold";
+    }
+}
+
+console.log(checkTemperature(35)); // Hot
+console.log(checkTemperature(25)); // Cold
+
+    
